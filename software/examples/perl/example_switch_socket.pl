cb55cfae0a11b599b8031531bf3c4ca3d0a87fc7
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-01-10 16:01:26 +0100

    Add Perl examples

---
+++ software/examples/perl/example_switch_socket.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl  
+
+use Tinkerforge::IPConnection;
+use Tinkerforge::BrickletRemoteSwitch;
+
+use constant HOST => 'localhost';
+use constant PORT => 4223;
+use constant UID => 'ufe'; # Change to your UID
+
+my $ipcon = IPConnection->new(); # Create IP connection
+my $rs = BrickletRemoteSwitch->new(&UID, $ipcon); # Create device object
+
+$ipcon->connect(&HOST, &PORT); # Connect to brickd
+# Don't use device before ipcon is connected
+
+# Switch socket with house code 42 and receiver code 23 on
+$rs->switch_socket(42, 23, $rs->SWITCH_TO_ON);
+
+print "\nPress any key to exit...\n";
+<STDIN>;
+$ipcon->disconnect();
+
