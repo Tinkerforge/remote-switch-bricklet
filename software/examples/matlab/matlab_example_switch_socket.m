d5ea8de453f3478592a3f57f4e626baca14a85f8
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-03-27 16:02:05 +0100

    matlab: add examples

---
+++ software/examples/matlab/matlab_example_switch_socket.m
@@ -0,0 +1,24 @@
+function matlab_example_switch_socket
+    import com.tinkerforge.IPConnection;
+    import com.tinkerforge.BrickletRemoteSwtich;
+
+    HOST = 'localhost';
+    PORT = 4223;
+    UID = 'jzG'; % Change to your UID
+    
+    ipcon = IPConnection(); % Create IP connection
+    rs = BrickletRemoteSwitch(UID, ipcon); % Create device object
+
+    ipcon.connect(HOST, PORT); % Connect to brickd
+    % Don't use device before ipcon is connected
+
+    % Switch socket with house code 17 and receiver code 1 on.
+    % House code 17 is 10001 in binary (least-significant bit first)
+    % and means that the DIP switches 1 and 5 are on and 2-4 are off.
+    % Receiver code 1 is 10000 in binary (least-significant bit first)
+    % and means that the DIP switch A is on and B-E are off.
+    rs.switchSocketA(0, 31, BrickletRemoteSwtich.SWITCH_TO_ON);
+
+    input('\nPress any key to exit...\n', 's');
+    ipcon.disconnect();
+end
---
+++ software/examples/matlab/octave_example_switch_socket.m
@@ -0,0 +1,23 @@
+function octave_example_switch_socket
+    more off;
+    
+    HOST = "localhost";
+    PORT = 4223;
+    UID = "jzG"; % Change to your UID
+
+    ipcon = java_new("com.tinkerforge.IPConnection"); % Create IP connection
+    rs = java_new("com.tinkerforge.BrickletRemoteSwitch", UID, ipcon); % Create device object
+
+    ipcon.connect(HOST, PORT); % Connect to brickd
+    % Don't use device before ipcon is connected
+
+    % Switch socket with house code 17 and receiver code 1 on.
+    % House code 17 is 10001 in binary (least-significant bit first)
+    % and means that the DIP switches 1 and 5 are on and 2-4 are off.
+    % Receiver code 1 is 10000 in binary (least-significant bit first)
+    % and means that the DIP switch A is on and B-E are off.
+    rs.switchSocketA(0, 31, rs.SWITCH_TO_ON);
+
+    input("\nPress any key to exit...\n", "s");
+    ipcon.disconnect();
+end
