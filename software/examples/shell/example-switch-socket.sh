9882af33d7f4f8aff4f984dd474b5f89ce432d93
Matthias Bolte <matthias@tinkerforge.com>
2017-04-20 20:10:17 +0200

    shell: Adapt example

--- software/examples/shell/example-switch-socket.sh
+++ software/examples/shell/example-switch-socket.sh
@@ -8,4 +8,4 @@ uid=XYZ # Change XYZ to the UID of your Remote Switch Bricklet
 # and means that the DIP switches 1 and 5 are on and 2-4 are off.
 # Receiver code 1 is 10000 in binary (least-significant bit first)
 # and means that the DIP switch A is on and B-E are off.
-tinkerforge call remote-switch-bricklet $uid switch-socket-a 17 1 on
+tinkerforge call remote-switch-bricklet $uid switch-socket-a 17 1 switch-to-on
